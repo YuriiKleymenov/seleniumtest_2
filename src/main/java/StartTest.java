55332857b19f71d3d347b6fb1b0e8c380d4e0f0d
Клеймёнов_ЮВ <Kleymenov.93@mail.ru>
2026-05-14 20:51:41 +0400

    изменил текст в println

--- src/main/java/StartTest.java
+++ src/main/java/StartTest.java
@@ -1,5 +1,5 @@
 public class StartTest {
     public static void main(String[] args) {
-        System.out.println("Тренировочный экземпляр");
+        System.out.println("Привет Мир...попробуем поменять это");
     }
 }
